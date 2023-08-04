06d395be94845cc54046e007bebb98a71d019c12
belaekaputri <belaekaputri28@gmail.com>
2023-08-04 16:32:36 +0700

    commit

---
+++ my-porfolio-main/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+     <Home> </Home>
+     <Portfolio> </Portfolio>
+  </div>
+</template>
+
+<script>
+import Home from './components/MyIdentity.vue'
+import Portfolio from './components/MyPortfolio.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+    Portfolio
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+}
+
+</style>
---
+++ my-porfolio-main/src/components/MyPortfolio.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <h3 class="text-center mt-3 mb-3">My Portfolio</h3>
+
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+      <div class="col">
+        <div class="card shadow">
+          <img src="../assets/gambar/p1.jpg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Title</h5>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow">
+          <img src="../assets/gambar/p2.jpg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Title</h5>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow">
+          <img src="../assets/gambar/p3.jpg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Title</h5>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card shadow">
+          <img src="../assets/gambar/p4.jpg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Title</h5>
+            <p class="card-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap");
+h3 {
+  font-family: "Playfair Display", serif;
+  font-size: 35px;
+  padding: 5px;
+  border-bottom: 2px solid black;
+}
+.card {
+  border: 1px solid black;
+}
+</style>
